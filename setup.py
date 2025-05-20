077292e0e3df39038dc4d4b9a507b0bf099b78d5
Daniel Gormezano <dangorme@gmail.com>
2025-05-20 14:17:34 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name = "vascular-classifier",                  
+    version = "0.1.0",
+    author = "Daniel Gormezano",
+    author_email = "dangorme@gmail.com",
+    description = "Pathomics and transcriptomics vascular classifier with Streamlit UI",
+    long_description = open("README.md").read(),
+    long_description_content_type = "text/markdown",
+    license = "MIT",
+    packages = find_packages(),                    
+    include_package_data = True,                  
+    install_requires = ["streamlit", "pandas", "numpy", "matplotlib", "scikit-learn", "xgboost", "openpyxl", "joblib"],
+    entry_points = {"console_scripts": ["vascular-classifier-app = vascular_classifier.app:main",],},
+    package_data = {"vascular_classifier": ["assets/*"],},
+    classifiers = ["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",],)
